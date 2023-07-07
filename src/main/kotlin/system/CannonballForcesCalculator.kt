303471663178945689711d6f0171b52c4687b979
tdallas <tdallas@itba.edu.ar>
2023-07-07 20:43:50 -0300

    Modified forces calculator, seems fine

--- src/main/kotlin/system/CannonballForcesCalculator.kt
+++ src/main/kotlin/system/CannonballForcesCalculator.kt
@@ -8,11 +8,14 @@ import kotlin.math.pow
 
 class CannonballForcesCalculator() : ForcesCalculator {
 
-    override fun getForces(particle: Particle, neighbours: List<Particle>, walls: List<Wall>): Vector {
-        val g = 9.81  // Aceleración debido a la gravedad (en m/s^2)
-        val gravityForce = Vector(0.0, 0.0, -particle.mass * g)  // La fuerza de gravedad actúa en la dirección -z
+    private fun calculateGravityForce(particle: Particle): Vector {
+        val g = 9.81  // Acceleration due to gravity (in m/s^2)
+        return Vector(0.0, 0.0, -particle.mass * g)  // Gravity force acts in the -z direction
+    }
 
+    private fun calculateParticleInteractionForce(particle: Particle, neighbours: List<Particle>): Vector {
         var interactionForce = Vector()
+
         for (otherParticle in neighbours) {
             if (particle != otherParticle) {
                 val normalDirection = (otherParticle.position - particle.position).normalize()
@@ -20,12 +23,37 @@ class CannonballForcesCalculator() : ForcesCalculator {
                 val normalVelocity = normalDirection * (relativeVelocity dot normalDirection)
                 val tangentialVelocity = relativeVelocity - normalVelocity
 
-                // La fuerza de fricción es proporcional a la velocidad relativa en la dirección tangencial
-                interactionForce += tangentialVelocity * particle.frictionCoefficient
+                // Friction force is proportional to the relative velocity in the tangential direction
+                interactionForce += tangentialVelocity * -particle.frictionCoefficient
             }
         }
 
-        return gravityForce + interactionForce
+        return interactionForce
     }
+
+    private fun calculateWallForce(particle: Particle, walls: List<Wall>): Vector {
+        var wallForce = Vector()
+
+        for (wall in walls) {
+            if (wall.overlapsWith(particle.position, particle.radius)) {
+                val normalDirection = wall.getNormal()
+                val normalVelocity = normalDirection * (particle.velocity dot normalDirection)
+
+                // The wall applies a force that is proportional to the particle's velocity in the direction of the wall's normal, also the -2 is used to simulate a rebound, -1 if we only want to invert sign
+                wallForce += normalVelocity.times(-2.0) * particle.frictionCoefficient
+            }
+        }
+
+        return wallForce
+    }
+
+    override fun getForces(particle: Particle, neighbours: List<Particle>, walls: List<Wall>): Vector {
+        val gravityForce = calculateGravityForce(particle)
+        val interactionForce = calculateParticleInteractionForce(particle, neighbours)
+        val wallForce = calculateWallForce(particle, walls)
+
+        return gravityForce + interactionForce + wallForce
+    }
+
 
 }
