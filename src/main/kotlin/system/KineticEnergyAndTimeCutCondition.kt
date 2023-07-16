3a85a4a48f27f1d59d8ea5ed3f5fad3f023dfd6c
tdorado <tdorado@itba.edu.ar>
2023-07-16 00:53:29 -0300

    fix energy cuttof on ball

--- src/main/kotlin/system/KineticEnergyAndTimeCutCondition.kt
+++ src/main/kotlin/system/KineticEnergyAndTimeCutCondition.kt
@@ -8,12 +8,12 @@ import kotlin.math.abs
 class KineticEnergyAndTimeCutCondition(
     private val energyThreshold: Double,
     timeToCut: Double,
+    private val minimumTime: Double,
+    private val averageSize: Int,
     private val shouldLog: Boolean = false,
 ) : TimeCutCondition(timeToCut) {
     private val logger = KotlinLogging.logger {}
     private val lastTenKineticEnergies = mutableListOf<Double>()
-    private val listSize = 100
-    private val minTime = 0.4
 
     override fun isFinished(particles: Set<Particle>, time: Double): Boolean {
         if (super.isFinished(particles, time)) {
@@ -21,24 +21,24 @@ class KineticEnergyAndTimeCutCondition(
         }
         val kineticEnergy = particles.map { it.getKineticEnergy() }.reduce { acc, kineticEnergy -> acc + kineticEnergy }
 
-        if (lastTenKineticEnergies.size < listSize) {
+        if (lastTenKineticEnergies.size < averageSize) {
             lastTenKineticEnergies.add(kineticEnergy)
-        } else if (lastTenKineticEnergies.size == listSize) {
+        } else if (lastTenKineticEnergies.size == averageSize) {
             if (shouldStopByVariationOfLastKs(kineticEnergy)) {
                 if (shouldLog) {
                     logger.info("Stopped by variation of last kinetic energies")
                 }
-                return time > minTime
+                return time > minimumTime
             }
             lastTenKineticEnergies.removeFirst()
             lastTenKineticEnergies.add(kineticEnergy)
         }
 
-        return time > minTime && kineticEnergy <= energyThreshold
+        return time > minimumTime && kineticEnergy <= energyThreshold
     }
 
     private fun shouldStopByVariationOfLastKs(actualK: Double): Boolean {
-        val average = lastTenKineticEnergies.sum() / listSize
+        val average = lastTenKineticEnergies.sum() / averageSize
         if (shouldLog) {
             logger.info("Average energy: $average")
         }
