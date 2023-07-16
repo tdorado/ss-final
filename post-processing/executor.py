cd80544090058388abe3d60aff20786108654205
tdorado <tdorado@itba.edu.ar>
2023-07-16 01:10:07 -0300

    added gamma missing

--- post-processing/executor.py
+++ post-processing/executor.py
@@ -16,7 +16,7 @@ def execute_dimeters_variation():
         execute_variation(f"-pld {diameters_range[0]} -pud {diameters_range[1]}", diameters_range[2])
 
 def execute_gammas_variation():
-    params = [ [20.0, "g_20"], [30.0, "g_30"],[40.0, "g_40"],[50.0, "g_50"],[60.0, "g_60"],[70.0, "g_70"],[80.0, "g_80"],[90.0, "g_90"],[100.0, "g_100"]]
+    params = [ [10.0, "g_10"], [20.0, "g_20"], [30.0, "g_30"],[40.0, "g_40"],[50.0, "g_50"],[60.0, "g_60"],[70.0, "g_70"],[80.0, "g_80"],[90.0, "g_90"],[100.0, "g_100"]]
     for gamma in params:
         execute_variation(f"-pGamma {gamma[0]}",  gamma[1])
 
