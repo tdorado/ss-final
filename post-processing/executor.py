a3283dfe2290215e99283790758a06bba4186822
tdorado <tdorado@itba.edu.ar>
2023-07-17 17:58:21 -0300

    fixed executor

--- post-processing/executor.py
+++ post-processing/executor.py
@@ -6,13 +6,13 @@ REPETITIONS = 5
 
 def execute_variation(variation_string, variation_name):
     for i in tqdm(range(REPETITIONS)):
-        print("Started simulating variation: " + variation_name + "...")
+        print("Started simulating variations for: " + variation_name + "...")
         cmd = f"java -jar build/libs/ss-final-1.0.jar -o {variation_name}_rep_{i} {variation_string}"
         os.system(cmd)
 
 
-def execute_dimeters_variation():
-    params = [[0.008, 0.012, "d1"], [0.012, 0.016, "d2"], [0.016, 0.02, "d3"]]
+def execute_diameters_variation():
+    params = [[0.008, 0.012, "d1"], [0.012, 0.015, "d2"], [0.015, 0.03, "d3"]]
     for diameters_range in params:
         execute_variation(f"-pld {diameters_range[0]} -pud {diameters_range[1]}", diameters_range[2])
 
@@ -24,8 +24,8 @@ def execute_gammas_variation():
         execute_variation(f"-pGamma {gamma[0]}", gamma[1])
 
 
-def execute_nParticles_variation():
-    params = [[1500, "nParticles_1500"]]
+def execute_number_of_particles_variation():
+    params = [[2000, "nParticles_2000"], [3000, "nParticles_3500"], [4500, "nParticles_4500"]]
     for nParticle in params:
         execute_variation(f"-n {nParticle[0]}", nParticle[1])
 
@@ -36,6 +36,7 @@ def execute_angles_variation():
         execute_variation(f"-ballAngle {angle[0]}", angle[1])
 
 
-execute_nParticles_variation()
+execute_diameters_variation()
+execute_gammas_variation()
+execute_number_of_particles_variation()
 execute_angles_variation()
-execute_dimeters_variation()
